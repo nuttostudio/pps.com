fd73ed06b3a1394cafbf369ff04b6a4aa7221b32
nusran14 <44315271+nusran14@users.noreply.github.com>
2018-10-20 17:17:08 +0700

    Update home.php

--- home.php
+++ home.php
@@ -1,7 +1,7 @@
 <div class="container">
 	<div class="row">
 		<div class="col-12">PPS</div>
-		<div class="col"><a href="./login">login</a></div>
+		<div class="col"><a href="./?page22=login">login</a></div>
 		<div class="col"><a href="./register">register</a></div>
 	</div>
 </div>
