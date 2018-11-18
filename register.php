6d573662c03fbf57beab4326ad841df84fa9b6f8
nusran14 <44315271+nusran14@users.noreply.github.com>
2018-11-18 19:22:24 +0700

    pps/18/11/18
    แก้ใขหน้าสมัครสมาชิก

--- register.php
+++ register.php
@@ -6,7 +6,6 @@
 		</div>
 		<div class="col-md-12 mt-3">
 			<form>
-				<h4>ข้อมูลส่วนบุคคล</h4>
 				<div class="form-group row">
 					<div class="col-lg-6 mt-3">
 						<input type="text" class="form-control" name="txt_Fname" placeholder="ชื่อ">
@@ -14,6 +13,24 @@
 					<div class="col-lg-6 mt-3">
 						<input type="text" class="form-control" name="txt_Lname" placeholder="สกุล">
 					</div>
+				</div>
+				<div class="form-group">
+					<input type="text" class="form-control" name="txt_Email" placeholder="E-mail">
+				</div>
+				<div class="form-group">
+					<input type="Password" class="form-control" name="txt_Pass" placeholder="Password">
+				</div>
+				<div class="form-group row">
+					<div class="col-lg-2">
+						<input type="date" class="form-control" name="txt_date">
+					</div>	
+				</div>
+				<div class="form-group">
+					<input type="radio" name="txt_sex" value="1">ชาย
+					<input type="radio" name="txt_sex" value="2">หญิง
+				</div>
+				<div class="form-group">
+					<button type="button" class="btn btn-primary btn-lg btn-block">สมัครสมาชิก</button>
 				</div>
 			</form>
 		</div>
